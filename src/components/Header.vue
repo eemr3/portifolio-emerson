1316caaf8544af93d06710e390212994079e6cd9
Emerson Moreira <eemr3@yahoo.com.br>
2021-05-28 23:44:55 -0300

    fix: ajusta texto header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
     <div class="flex justify-center items-center flex-col sm:w-4/12">
       <h1 class="text-white sm:text-6xl my-5">Emerson Moreira</h1>
       <div class="border-b-2 border-white w-full my-3"></div>
-      <h2 class="text-white sm:text-4xl mt-3">Web developer</h2>
+      <h2 class="text-white sm:text-4xl mt-3">Desenvolvedor Web</h2>
     </div>
   </header>
 </template>
